cd8387110f73ad714ee85006d9c3799998f1eda2
Vinncent Alexander Wong <vinncentwong2@gmail.com>
2022-07-17 14:30:11 +0700

    add new styling to several components

--- src/Components/Navbar/NavbarTwo.tsx
+++ src/Components/Navbar/NavbarTwo.tsx
@@ -3,13 +3,13 @@
 export const NavbarTwo = (): JSX.Element => {
     return (
         <div id = "navbar-2">
-            <a href = "https://www.google.com"><span>Categories</span></a>
-            <a href = "https://www.google.com"><span>Brands</span></a>
-            <a href = "https://www.google.com"><span>Promotions</span></a>
-            <a href = "https://www.google.com"><span>Beauty Hacks</span></a>
-            <a href = "https://www.google.com"><span>New Arrivals</span></a>
-            <a href = "https://www.google.com"><span>Sale</span></a>
-            <a href = "https://www.google.com" id = "drop-my-empty-a"><span>Drop my empty</span></a>
+            <a href = "https://www.google.com"><span className = "a">Categories</span></a>
+            <a href = "https://www.google.com"><span className = "a">Brands</span></a>
+            <a href = "https://www.google.com"><span className = "a">Promotions</span></a>
+            <a href = "https://www.google.com"><span className = "a">Beauty Hacks</span></a>
+            <a href = "https://www.google.com"><span className = "a">New Arrivals</span></a>
+            <a href = "https://www.google.com"><span className = "a">Sale</span></a>
+            <a href = "https://www.google.com" id = "drop-my-empty-a"><span className = "a">Drop my empty</span></a>
         </div>
     );
 }
--- src/Pages/RegisterPage.tsx
+++ src/Pages/RegisterPage.tsx
@@ -13,33 +13,33 @@ export const RegisterPage = () => {
             <div id = "register-page">
                 <div className="content-register-box">
                     <Box component="form" className = "box-register">
-                        <h3>Create your account</h3>
-                        <p>It's free and easy</p>
-                        <div className = "register-box">
-                            <FormControlComponent variant = "filled" id = "input-firstname" type = "text" className="form-control" label="First Name*" placeHolder="Enter your first name"></FormControlComponent>
-                            <FormControlComponent variant = "filled" id = "input-lastname"type = "text" className="form-control" label="Last Name*" placeHolder="Enter your last name"></FormControlComponent>
-                        </div>
-                        <div className = "register-box">
-                            <FormControlComponent variant = "filled" id = "input-email" type = "email" className="form-control-email" label="Email*" placeHolder="Enter your email"></FormControlComponent>
-                        </div>
-                        <div className = "register-box">
-                            <FormControlComponent variant = "filled" id = "input-password" type = "password" className="form-control" label="Password*" placeHolder="Enter your password"></FormControlComponent>
-                            <FormControlComponent variant = "filled" id = "input-confirm-password" type = "password" className="form-control" label="Confirm Password*" placeHolder="Enter your password again"></FormControlComponent>
-                        </div>
-                        <div className = "register-box">
-                            <ComboBoxComponents label="Gender*" className = "form-gender"></ComboBoxComponents>
-                            <FormLabel htmlFor="date-input" id = "date-label">Birth date*</FormLabel>
-                            <div></div>
-                            <FormControlComponent variant = "filled" id = "date-input" type = "date" className="form-date" placeHolder="DD/MM/YYYY" label=""></FormControlComponent>
-                        </div>
-                        <div className = "register-box">
-                            <Checkbox className = "check-box"></Checkbox>
-                            <p>By checking the box, I agree that I have read and accepted the <a href = "https://www.google.com">Terms of Use</a> and <a href = "https://www.google.com">Privacy and Policy.</a></p>
-                        </div>
-                        <div className = "register-box">
-                            <p>Already have account? <a href = "https://www.google.com">Sign In</a></p>
-                            <ButtonComponent variant="contained">Create Account</ButtonComponent>
-                        </div>
+                            <h3 id = "create-account-text">Create your account</h3>
+                            <p id = "free-easy-text">It's free and easy</p>
+                            <div className = "register-box" id = "input-1">
+                                <FormControlComponent variant = "filled" id = "input-firstname" type = "text" className="form-control" label="First Name*" placeHolder="Enter your first name"></FormControlComponent>
+                                <FormControlComponent variant = "filled" id = "input-lastname"type = "text" className="form-control" label="Last Name*" placeHolder="Enter your last name"></FormControlComponent>
+                            </div>
+                            <div className = "register-box" id = "input-2">
+                                <FormControlComponent variant = "filled" id = "input-email" type = "email" className="form-control-email" label="Email*" placeHolder="Enter your email"></FormControlComponent>
+                            </div>
+                            <div className = "register-box" id = "input-3">
+                                <FormControlComponent variant = "filled" id = "input-password" type = "password" className="form-control" label="Password*" placeHolder="Enter your password"></FormControlComponent>
+                                <FormControlComponent variant = "filled" id = "input-confirm-password" type = "password" className="form-control" label="Confirm Password*" placeHolder="Enter your password again"></FormControlComponent>
+                            </div>
+                            <div className = "register-box" id = "input-4">
+                                <ComboBoxComponents label="Gender*" className = "form-gender"></ComboBoxComponents>
+                                <FormLabel htmlFor="date-input" id = "date-label">Birth date*</FormLabel>
+                                <div></div>
+                                <FormControlComponent variant = "filled" id = "date-input" type = "date" className="form-date" placeHolder="DD/MM/YYYY" label=""></FormControlComponent>
+                            </div>
+                            <div className = "register-box" id = "input-5">
+                                <Checkbox className = "check-box"></Checkbox>
+                                <p>By checking the box, I agree that I have read and accepted the <a href = "https://www.google.com">Terms of Use</a> and <a href = "https://www.google.com">Privacy and Policy.</a></p>
+                            </div>
+                            <div className = "register-box" id = "input-6">
+                                <p>Already have account? <a href = "https://www.google.com">Sign In</a></p>
+                                <ButtonComponent variant="contained">Create Account</ButtonComponent>
+                            </div>
                     </Box>             
                 </div>
             </div>
