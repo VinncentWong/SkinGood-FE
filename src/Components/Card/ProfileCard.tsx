a4e14ba619bbd583a9a8412cf6f2f0fe05464db4
Vinncent Alexander Wong <vinncentwong2@gmail.com>
2022-07-22 21:46:03 +0700

    add card styling

--- src/Components/Card/ProfileCard.tsx
+++ src/Components/Card/ProfileCard.tsx
@@ -1,6 +1,9 @@
 import React, { ReactEventHandler } from "react"
 import { ButtonComponent } from "../Button/ButtonComponent"
 
+const logo = require("../../Assets/Panah.png");
+// const cameraLogo = require("../../Assets/Vector.png");
+
 export const ProfileCard = (): JSX.Element => {
 
     const buttonEditEmailHandler: ReactEventHandler = (event: React.FormEvent) => {
@@ -15,46 +18,51 @@ export const ProfileCard = (): JSX.Element => {
 
     }
     return(
-        <div>
-            <img alt="profile"></img>
-            <div>
-                <div>
-                    <p>First Name</p>
-                    <div>
-                        BCC Soto
+        <div id = "profile-card">
+            <img alt="profile" id = "profile-picture"></img>
+            {/* <img alt="camera" id = "camera"></img> */}
+            <div id = "profile-1">
+                <div id = "div-first-name">
+                    <p><h3>First Name</h3></p>
+                    <div id = "first-name-value">
+                        <p>BCC Soto</p>
                     </div>
                 </div>
-                <div>
-                    <p>Last Name</p>
-                    <div>
-                        Sate
+                <div id = "div-last-name">
+                    <p><h3>Last Name</h3></p>
+                    <div id = "last-name-value">
+                        <p>Sate</p>
                     </div>
                 </div>
             </div>
-            <div>
-                <div>
-                    <label>Email</label>
+            <div id = "profile-2">
+                <div id = "div-email">
+                    <label id = "label-email-profile"><h3>Email</h3></label>
                     <ButtonComponent className="" id="button-edit-email" variant="contained" handler={buttonEditEmailHandler}>
-                        <p>bccsotosate@gmail.com</p>
+                        <p id = "email-value">bccsotosate@gmail.com</p>
+                        <img src = {logo} alt = "arrow" id = "arrow-1"></img>
                     </ButtonComponent>
                 </div>
-                <div>
-                    <label>Phone Number</label>
+                <div id = "div-phone">
+                    <label id = "label-email-phonenumber"><h3>Phone Number</h3></label>
                     <ButtonComponent className="" id="button-edit-phonenumber" variant="contained" handler={buttonEditPhoneNumber}>
-                        <p>Atur Sekarang</p>
+                        <p id = "phone-number-value">Atur Sekarang</p>
+                        <img src = {logo} alt = "arrow" id = "arrow-2"></img>
                     </ButtonComponent>
                 </div>
             </div>
-            <div>
-                <div>
-                    <p>Membership ID</p>
-                    <div>
-                        0888812312303881
+            <div id = "profile-3">
+                <div id = "div-membership">
+                    <h3>Membership ID</h3>
+                    <div id = "membership-container-value">
+                        <p id = "membership-value">0888812312303881</p>
                     </div>
                 </div>
-                <div>
-                    <p>Gender</p>
-                    <p>Male</p>
+                <div id = "div-gender">
+                    <h3>Gender</h3>
+                    <div id = "gender-value">
+                        <p>Male</p>
+                    </div>
                 </div>
             </div>
             <div>
@@ -67,6 +75,7 @@ export const ProfileCard = (): JSX.Element => {
                     <div>
                         <ButtonComponent className="" id="button-edit-password" variant="contained" handler={buttonEditPassword}>
                             <p>Atur Sekarang</p>
+                            <img src = {logo} alt = "arrow"></img>
                         </ButtonComponent>
                     </div>
                 </div>
--- src/Pages/AccoutPage.tsx
+++ src/Pages/AccoutPage.tsx
@@ -1,9 +1,11 @@
+import { ProfileCard } from "../Components/Card/ProfileCard"
 import { DropDown } from "../Components/Navbar/DropDown"
 
 export const AccountPage = (): JSX.Element => {
     return (
         <div>
             <DropDown></DropDown>
+            <ProfileCard></ProfileCard>
         </div>
     )
 }
