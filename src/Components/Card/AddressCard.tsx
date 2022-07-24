68fc9337b195de80064e05c80755edbd3a44e702
Vinncent Alexander Wong <vinncentwong2@gmail.com>
2022-07-24 19:05:23 +0700

    add new implementation to index.css and card

--- src/Components/Card/AddressCard.tsx
+++ src/Components/Card/AddressCard.tsx
@@ -1,3 +1,4 @@
+import { Divider } from "@mui/material";
 import { ButtonComponent } from "../Button/ButtonComponent"
 
 const plusLogo = require("../../Assets/black-plus.png");
@@ -7,6 +8,27 @@ export const AddressCard = (): JSX.Element => {
         <div id = "address-card">
             <h2>Alamat Saya</h2>
             <ButtonComponent id="tambah-alamat-baru" className="" handler={undefined as any} variant="contained"><Content></Content></ButtonComponent>
+            <div id = "user-address-content">
+                <Divider></Divider>
+                <div className = "container-address-user">
+                    <div className = "content-address">
+                        <p>Nama Lengkap</p>
+                        <p>BCC Soto Sate</p>
+                    </div>
+                    <div className="content-address">
+                        <p>Telepon</p>
+                        <p id = "phone-address-value">08565062108</p>
+                    </div>
+                    <div className="content-address" id = "address-card-container">
+                        <p>Alamat</p>
+                        <p id = "address-address-value">Jl. KertoRaharjo Dalam No 56, Lowokwaru, Malang, Jawa Timur, Indonesia AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</p>
+                    </div>
+                    <div>
+                        <p className = "one">1</p>
+                    </div>
+                </div>
+                <Divider></Divider>
+            </div>
         </div>
     )
 }
